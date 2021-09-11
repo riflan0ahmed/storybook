5b86df45e823465beee476ff8b0cff19d1e3397d
riflan <riflan0ahmed@gmail.com>
2021-09-11 23:04:23 +0530

    change task background to normal

--- src/components/Task.js
+++ src/components/Task.js
@@ -39,7 +39,7 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
-          style={{ background: "red" }}
+          // style={{ background: "red" }}
         />
       </div>
 
